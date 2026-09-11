48839315fe4c614f41a7e677081a25cc34bab9eb
Azzks <alexclerwood@gmail.com>
2026-09-11 16:03:17 +0000

    ajout effet de rideau a chaque changement de page

--- app/template.tsx
+++ app/template.tsx
@@ -7,32 +7,6 @@ export default function Template({
 }: {
   children: React.ReactNode;
 }) {
-  const [visible, setVisible] = useState(false);
-
-  useEffect(() => {
-    const id = requestAnimationFrame(() => setVisible(true));
-    return () => cancelAnimationFrame(id);
-  }, []);
-
-  return (
-    <div
-      className={`transition-all duration-500 ease-out ${
-        visible ? "translate-y-0 opacity-100" : "translate-y-2 opacity-0"
-      }`}
-    >
-      {children}
-    </div>
-  );
-}
-"use client";
-
-import { useEffect, useState } from "react";
-
-export default function Template({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
   const [revealed, setRevealed] = useState(false);
 
   useEffect(() => {
